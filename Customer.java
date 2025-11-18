4c5fb36b8962ba16c2e8960804adf746092a9810
WiniciusSantosLoebens <wsloebens@minha.fag.edu.br>
2025-11-18 23:25:24 +0000

    COMMIT 2

--- Customer.java
+++ Customer.java
@@ -25,22 +25,8 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        
+        thisAmount = amountFor(each);
 
         frequentRenterPoints ++;
         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
@@ -56,4 +42,23 @@ public class Customer {
              " frequent renter points";
      return result;
    }
+}
+private double amountFor(Rental aRental) {
+   double thisAmount = 0;
+   switch (aRental.getMovie().getPriceCode()) {
+      case Movie.REGULAR:
+         thisAmount += 2;
+         if (aRental.getDaysRented() > 2)
+            thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+         break;
+      case Movie.NEW_RELEASE:
+         thisAmount += aRental.getDaysRented() * 3;
+         break;
+      case Movie.CHILDRENS:
+         thisAmount += 1.5;
+         if (aRental.getDaysRented() > 3)
+            thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+          break;
+   }
+   return thisAmount;
 }
