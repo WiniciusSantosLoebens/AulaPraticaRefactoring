ff58b6a14a1b427e7608defeadff0ba17c46bc19
WiniciusSantosLoebens <wsloebens@minha.fag.edu.br>
2025-11-25 22:33:29 +0000

    COMMIT 9

--- Customer.java
+++ Customer.java
@@ -38,11 +38,9 @@ public class Customer {
       String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
-         // mostra valores para este aluguel
          result += each.getMovie().getTitle()+ ": " +
                    String.valueOf(each.getCharge()) + "<BR>\n";
       }
-      // rodapé
       result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
       result += "On this rental you earned <EM>" +
           String.valueOf(getTotalFrequentRenterPoints()) +
